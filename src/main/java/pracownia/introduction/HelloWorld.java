9e0b56ace958f6e477cf63fb4b24ca740b97569f
s400748 <aurebrus@gmail.com>
2020-02-29 11:02:39 +0100

    first

--- src/main/java/pracownia/introduction/HelloWorld.java
+++ src/main/java/pracownia/introduction/HelloWorld.java
@@ -1,8 +1,11 @@
 package pracownia.introduction;
 
-public class HelloWorld {
+import org.apache.log4j.Logger;
 
+public class HelloWorld {
     public static void main(String [ ] args) {
+        Logger log = Logger.getLogger("name");
         System.out.println("Hello World");
+        log.info("message");
     }
 }
